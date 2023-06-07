606b2970b18bd569f49369d5f9d5cb20745a0857
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-06 22:57:12 -0300

    cria primeiro componente container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
+import GlobalStyle from "../src/styles/global.js"
+import style from "styled-components"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const Container = style.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Container>
+
+      </Container>
+      <ToastContainer autoClose={300} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
 }
 
