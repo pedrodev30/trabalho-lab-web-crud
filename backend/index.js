5e5c004906773c663a915721e42395ff85e34c88
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 19:24:44 -0300

    importa usersRoutes no backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import getUsers from "./routes/users.js"
+import usersRoutes from "./routes/users.js"
 import cors from "cors"
 
 const app = express()
@@ -7,6 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/", getUsers)
+app.use("/", usersRoutes)
 
 app.listen(8800)
