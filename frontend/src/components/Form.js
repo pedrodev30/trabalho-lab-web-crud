7a4f69d41c1ec2302f53792306069fb67fd2a6f3
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 12:10:52 -0300

    :lipstick: feat: adiciona estilização aos inputs do form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -12,11 +12,36 @@ const FormContainer = styled.form`
   border-radius: 5px;
 `
 
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`
+
+const Label = styled.label``
+
 const Form = ({ onEdit }) => {
   const ref = useRef()
 
   return (
     <FormContainer ref={ref}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome"></Input>
+        <Label>E-mail</Label>
+        <Input name="email" type="email"></Input>
+        <Label>Telefone</Label>
+        <Input name="telefone"></Input>
+        <Label>Data de Nascimento</Label>
+        <Input name="data_nascimento" type="date"></Input>
+      </InputArea>
     </FormContainer>
   )
 }
