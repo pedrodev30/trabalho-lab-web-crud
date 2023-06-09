afac4a75a9e9e324e2b3e18d9c9c050ff9d48038
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 11:36:39 -0300

    :lipstick: feat: aplica estilização ao formulario

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,24 @@
+import React, { useRef } from "react"
+import styled from "styled-components"
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`
+
+const Form = ({ onEdit }) => {
+  const ref = useRef()
+
+  return (
+    <FormContainer ref={ref}>
+    </FormContainer>
+  )
+}
+
+export default Form
