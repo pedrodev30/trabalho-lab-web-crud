a9f45ec4d6b9a51db714318bf613c4975668785f
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 14:36:55 -0300

    :lipstick: feat: adiciona estilização ao thead da tabela de usuários

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -12,9 +12,33 @@ const Table = styled.table`
   word-wrap: break-word;
 `
 
+const Thead = styled.th``
+
+const Tr = styled.tr``
+
+const Td = styled.td`
+  text-align: center;
+  border-bottom: inset;
+  padding-bottom: 5px;
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyweb && "display: none"}
+  }
+`
+
 const Grid = () => {
   return(
-    <Table></Table>
+    <Table>
+      <Thead>
+        <Tr>
+          <Td>Nome</Td>
+          <Td>E-mail</Td>
+          <Td onlyweb>Telefone</Td>
+          <Td></Td>
+          <Td></Td>
+        </Tr>
+      </Thead>
+    </Table>
   )
 }
 
