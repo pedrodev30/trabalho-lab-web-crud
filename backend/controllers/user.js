09d18ce1017d9aed3aa7a4d72e5688c7def75638
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 16:13:40 -0300

    :sparkles: feat: cria funcionalidade de inserir dados no backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,3 +9,21 @@ export const getUsers = (_, res) => {
     return res.status(200).json(data)
   })
 }
+
+export const addUser = (req, res) => {
+  const q =
+    "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)"
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.telefone,
+    req.body.data_nascimento,
+  ]
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err)
+
+    return res.status(200).json("Usuário criado com sucesso.")
+  })
+}
