4f03a4a49db6f211267fae36326a5a38cb280189
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-09 14:27:28 -0300

    :lipstick: feat: adiciona estilização na tabela de usuários

---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,21 @@
+import React from "react"
+import styled from "styled-components"
+
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 800px;
+  margin: 20px auto;
+  word-wrap: break-word;
+`
+
+const Grid = () => {
+  return(
+    <Table></Table>
+  )
+}
+
+export default Grid
