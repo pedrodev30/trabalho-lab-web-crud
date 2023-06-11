bc857f52d7839bbf55ce27e16b60f93548487e2b
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-06-11 09:18:18 -0300

    criação de variaveis de cores no

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,6 +2,21 @@ import { createGlobalStyle } from "styled-components"
 
 const Global = createGlobalStyle`
 
+  :root {
+    --bg-body: #f2f2f2;
+    --color-text: #000;
+    --bg-form-table: #fff;
+    --bg-button: #2c73d2;
+    --bg-input: #fff;
+  }
+
+  .dark-theme {
+    --bg-body: #292C35;
+    --color-text: #fff;
+    --bg-form-table: #111;
+    --bg-input: #111;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -13,7 +28,7 @@ const Global = createGlobalStyle`
     height: 100vh;
     display: flex;
     justify-content: center;
-    background-color: #f2f2f2;
+    background-color: #f1f1f1;
   }
 `
 
